bff9b3c7c79e28f6497d70ec97ec48c36b4f3d8a
Keiron-Beadle <K.BEADLE-2018@hull.ac.uk>
2022-03-15 13:10:29 +0000

    Added a basic folder structure so that we can go in better organised.

--- Assets/ShowcaseBranches.cs
+++ Assets/ShowcaseBranches.cs
@@ -7,7 +7,7 @@ public class ShowcaseBranches : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //My fancy code
     }
 
     // Update is called once per frame
