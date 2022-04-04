622d6bc260d96e02023e2ae00ed3ae41664e78d6
Humza Khan <humza.khan890@gmail.com>
2022-04-04 08:28:43 +0100

    Moved event initialising to Awake method

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+
 public class HealthManager : MonoBehaviour
 {
     public int _health = 100;
-    public UnityEvent _HealthIsZero;
+    public IntEvent _healthChanged;
+    public UnityEvent _healthIsZero;
 
-    // Start is called before the first frame update
-    void Start()
+    // Runs before start() on all game objects
+    void Awake()
     {
-        _HealthIsZero = new UnityEvent();
+        _healthChanged = new IntEvent();
+        _healthIsZero = new UnityEvent();
     }
 
     public void ReduceHealth(int amount)
@@ -19,17 +22,12 @@ public class HealthManager : MonoBehaviour
         if (amount > 0)
         {
             _health -= amount;
+            _healthChanged.Invoke(_health);
         }
         if (amount == 0)
         {
             // Game over
-            _HealthIsZero.Invoke();
+            _healthIsZero.Invoke();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
