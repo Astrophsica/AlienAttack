2331bf4191ad441fca970862558ebd047ed334a1
Humza Khan <humza.khan890@gmail.com>
2022-04-04 07:50:52 +0100

    Updated "EnemyPathing" to update "HealthManager" object when enemy collides StrongHold

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -17,12 +17,14 @@ public class EnemyPathing : MonoBehaviour
     bool _reachedEndOfPath = false;
     Seeker _seeker; //Seeker component on AI
     LayerMask _strongholdLayer;
-    
+    HealthManager _healthManager;
+
     void Start()
     {
         _transform = GetComponent<Transform>();
         _seeker = GetComponent<Seeker>();
         _strongholdLayer = LayerMask.NameToLayer("Stronghold");
+        _healthManager = GameObject.Find("HealthManager").gameObject.GetComponent<HealthManager>();
         //Generate path
         if (_target is null) { return; }
         GenerateNewPath(_target);
@@ -43,8 +45,7 @@ public class EnemyPathing : MonoBehaviour
     {
         if (collision.gameObject.layer == _strongholdLayer)
         {
-            var stronghold_script = collision.GetComponent<HealthManager>();
-            stronghold_script.ReduceHealth(1);
+            _healthManager.ReduceHealth(1);
         }
     }
 
