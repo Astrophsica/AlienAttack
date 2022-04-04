babd57a312fab8bf4561173ffa9c2d5164840bcb
Humza Khan <humza.khan890@gmail.com>
2022-04-04 07:35:00 +0100

    Added _HealthIsZero event

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthManager : MonoBehaviour
 {
     int _health = 100;
+    public UnityEvent _HealthIsZero;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _HealthIsZero = new UnityEvent();
     }
 
     public void ReduceHealth(int amount)
     {
-        _health -= amount;
+        if (amount > 0)
+        {
+            _health -= amount;
+        }
+        if (amount == 0)
+        {
+            // Game over
+            _HealthIsZero.Invoke();
+        }
     }
 
     // Update is called once per frame
