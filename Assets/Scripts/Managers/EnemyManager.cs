561e2d32dedc6d3475209b2577c513fe6403c4d5
Humza Khan <humza.khan890@gmail.com>
2022-04-25 21:09:50 +0100

    Commented EnemyManager

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -4,8 +4,10 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
+// Author: Humza Khan
 public class EnemyManager : MonoBehaviour
 {
+    [Tooltip("List of unique enemies")]
     [SerializeField]
     GameObject[] EnemyTypes;
 
@@ -14,6 +16,7 @@ public class EnemyManager : MonoBehaviour
 
     static public GameObject SpawnNewEnemy(string type, Vector3 position)
     {
+        // Get enemy object to spawned based on type
         GameObject enemyObjectToSpawn = null;
         switch (type)
         {
@@ -22,6 +25,7 @@ public class EnemyManager : MonoBehaviour
                 break;
         }
         
+        // Create enemy and add to enemies list
         var enemy = Instantiate(enemyObjectToSpawn, position, Quaternion.identity);
         enemies.Add(enemy);
         return enemy;
@@ -29,18 +33,21 @@ public class EnemyManager : MonoBehaviour
 
     static public void DestroyEnemy(GameObject enemy)
     {
+        // Remove enemy from enemies list and then destroy enemy object
         enemies.Remove(enemy);
         Destroy(enemy);
     }
 
     static private GameObject LoadPrefabFromFile(string filename)
     {
+        // Check if enemy object already loaded in. If so, return that object
         GameObject loadedObject;
         if (enemyAssets.TryGetValue(filename, out loadedObject))
         {
             return loadedObject;
         }
 
+        // Get object from file, add to asset list and return object
         loadedObject = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Game/Enemy/" + filename + ".prefab", typeof(GameObject));
         if (loadedObject == null)
         {
