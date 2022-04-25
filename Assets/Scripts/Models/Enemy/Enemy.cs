e117740e420430279c485a0605c42834c498d392
Humza Khan <humza.khan890@gmail.com>
2022-04-25 21:01:23 +0100

    Removed _HealthManager as not used in Enemy.cs

--- Assets/Scripts/Models/Enemy/Enemy.cs
+++ Assets/Scripts/Models/Enemy/Enemy.cs
@@ -3,22 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Author: Humza Khan
 public class Enemy : MonoBehaviour
 {
+    [Tooltip("Movement speed of enemy")]
     [SerializeField]
     private float Speed;
+
+    [Tooltip("Health of enemy")]
     [SerializeField]
     private int Health;
 
     EnemyPathing _enemyPathing;
     LayerMask _strongholdLayer;
-    HealthManager _healthManager;
 
     void Awake()
     {
         _enemyPathing = new EnemyPathing(Speed, GetComponent<Transform>(), GetComponent<Seeker>());
         _strongholdLayer = LayerMask.NameToLayer("Stronghold");
-        _healthManager = GameObject.Find("HealthManager").gameObject.GetComponent<HealthManager>();
     }
     // Start is called before the first frame update
     void Start()
