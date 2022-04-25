07731a78367a5b738a032daed04e39f1b11ff2ba
Humza Khan <humza.khan890@gmail.com>
2022-04-25 20:48:36 +0100

    Added comments to ShopItem

--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Authors: Humza Khan and Keiron Beadle
 public class ShopItem : MonoBehaviour
 {
+    [Tooltip("Price of item")]
     [SerializeField]
     public int Price;
 
+    [Tooltip("Game object prefab for this item")]
     [SerializeField]
     public GameObject Prefab;
 
+    [Tooltip("The player link object with property to Player object")]
     [SerializeField]
     public GameObject PlayerLink;
 
     public void Start()
     {
+        // Sets OnClick to be called on button click
         gameObject.GetComponent<Button>().onClick.AddListener(OnClick);
     }
 
     public void OnClick()
     {
+        // Sets ObjectToPlace for placer script
         PlayerLink.GetComponent<PlayerLink>().Player.GetComponent<Placer>().ObjectToPlace = Prefab;
     }
 }
